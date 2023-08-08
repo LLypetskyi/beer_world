bf9311e7ec46dcf5a09a964d3cd4137486cf2d91
Lubomyr Lypetskyi <liubomyr.lypetskyi@gmail.com>
2023-08-09 00:41:33 +0300

    Initial commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HistoryComponent } from './history/history.component';
+import { BreweriesComponent } from './breweries/breweries.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: "", component: HistoryComponent },
+  { path: "breweries", component: BreweriesComponent },
+  { path: "**", component: NotFoundComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HistoryComponent } from './history/history.component';
+import { BreweriesComponent } from './breweries/breweries.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HistoryComponent,
+    BreweriesComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
